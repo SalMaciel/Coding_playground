22c0d2f2ea63550e0eb92891fd739e9cfbdcbb81
SalMaciel <sal.macielm@gmail.com>
2022-11-09 11:42:00 -0700

    update on r,p,s file

--- Python/if_rock_paper_scissors.py
+++ Python/if_rock_paper_scissors.py
@@ -11,17 +11,21 @@ options = ["r", "p", "s"]
 comp_choice = random.choice(options)
 
 # User selection
-user_choice = input("Make your choice: (r)ock, (p)aper, (s)cissors!")
+user_choice = input("Make your choice: (r)ock, (p)aper, (s)cissors!: ")
 
 # Game logic
 
 if comp_choice == user_choice:
     print("It's a tie!")
+
 elif comp_choice == "r" and user_choice == "p":
     print("You win!")
+
 elif comp_choice == "p" and user_choice == "s":
     print("You win!")
+
 elif comp_choice == "s" and user_choice == "r":
     print("You win!")
+
 else:
     print("You Lose!")
